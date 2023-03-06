249bdf8f160ad6d0996372e53a5dc08694fb9c5b
Silsempersil <126631220+Silsempersil@users.noreply.github.com>
2023-03-05 23:04:19 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let ocultarCampos_btn = document.getElementById ('ocultarCampos_btn')
+let ocultarcampos = document.getElementById ('ocultarCampos');
+ocultarCampos_btn.addEventListener('click', toggleText);
+function toggleText () {
+ocultarCampos.classList.toggle('show');
+
+}
+
